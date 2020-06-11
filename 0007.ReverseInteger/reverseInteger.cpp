0e55722667ddefe446fc5e25c43cb44896eabd3e
Villarealfan <18101339258@163.com>
2020-06-11 14:57:05 +0800

    数字

--- 0007.ReverseInteger/reverseInteger.cpp
+++ 0007.ReverseInteger/reverseInteger.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int reverse(int x) {
-        long res = 0;
+        long long res = 0;
         while (x != 0){
             res = res*10 + x%10;
             x /= 10;
