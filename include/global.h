f8e916249020e0641cf002e292537d77b8336a3a
Villarealfan <18101339258@163.com>
2020-08-13 07:44:13 +0800

    add treenode

--- include/global.h
+++ include/global.h
@@ -20,4 +20,11 @@ struct ListNode{
     ListNode(int x): val(x), next(NULL){}
 };
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 #endif //GLOBAL_H
